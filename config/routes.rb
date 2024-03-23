073fdadd139eb84b0f746ecd35d4b2c82a05d04c
AlirioDS <aliriodiaz111@gmail.com>
2024-03-23 16:36:17 -0300

    Resolve permissions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  } do
+    delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
 end
